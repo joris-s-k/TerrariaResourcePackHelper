261488b24cbd9042e2fe36f756eeece7319d0183
joris-s-k <joris-s-k@users.noreply.github.com>
2025-01-06 15:39:14 +0100

    failsafes for pack.json reading

--- init.py
+++ init.py
@@ -5,6 +5,15 @@ terrariaConfigDir = '/home/joris/.local/share/Terraria/'
 steamWorkshopDir = '/home/joris/.steam/debian-installation/steamapps/workshop/content/105600/'
 confDir = os.path.join(terrariaConfigDir, 'config.json')
 
+def getSafeFile(file) -> str:
+    fileStub = []
+    for line in file:
+        fileStub.append(line)
+        if 'Name' in line:
+            break
+    return ''.join(fileStub)+'"Dummy": ""}'
+    
+
 def listPacks(active):
     print('Printing Active Packs')
     if os.path.exists(confDir):
@@ -15,23 +24,22 @@ def listPacks(active):
         for pack in jsonData['ResourcePacks']:
             if pack['Enabled'] is active:
                 name = pack['FileName']
-                #print(name)
+                packName = name
                 jsonName = 'pack.json' if os.path.exists(os.path.join(steamWorkshopDir,name,'pack.json')) else 'Pack.json'
                 steamDir = os.path.join(steamWorkshopDir,name,jsonName)
                 localDir = os.path.join(terrariaConfigDir, 'ResourcePacks', name)
                 if os.path.exists(steamDir):
                     #print(steamDir)
                     #packName = json.load(open(steamDir, 'r'))['Name']
-                    fileStub = io.open(steamDir, 'r', encoding='utf-8-sig').readlines(2)
-                    fixedFile = ''.join(fileStub)+'"Dummy": ""}'
+                    fixedFile = getSafeFile(io.open(steamDir, 'r', encoding='utf-8-sig'))
                     packName = json.loads(fixedFile)['Name']
                     #packName = json.load(io.open(steamDir, 'r', encoding='utf-8-sig'),strict=False)['Name']
                 elif os.path.exists(localDir):
                     jsonName = 'pack.json' if os.path.exists(os.path.join(localDir,'pack.json')) else 'Pack.json'
-                    fileStub = io.open(os.path.join(localDir,jsonName), 'r', encoding='utf-8-sig').readlines(2)
-                    fixedFile = ''.join(fileStub) + '"Dummy": ""}'
-                    packName = json.loads(fixedFile)['Name']
-                    #packName = json.load(io.open(os.path.join(terrariaConfigDir,'ResourcePacks',name,jsonName), 'r', encoding='utf-8-sig'),strict=False)['Name']
+                    if os.path.exists(os.path.join(localDir,jsonName)):
+                        fixedFile = getSafeFile(io.open(os.path.join(localDir,jsonName), 'r', encoding='utf-8-sig'))
+                        packName = json.loads(fixedFile)['Name']
+                        #packName = json.load(io.open(os.path.join(terrariaConfigDir,'ResourcePacks',name,jsonName), 'r', encoding='utf-8-sig'),strict=False)['Name']                
                 else:
                     print(f'(ERROR): {name} not found on disk')
                 print(f'#{pack['SortingOrder']}\t- {packName}')

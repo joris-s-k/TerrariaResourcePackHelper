36078de1e153fc52f6dcdbe79da519da71e7e02a
joris-s-k <joris-s-k@users.noreply.github.com>
2025-12-29 18:22:34 +0100

    code review mit dem G
    <3

--- init.py
+++ init.py
@@ -1,73 +1,94 @@
-import os, sys, io
 import json
+import os
+from pathlib import Path
 from sys import exit
+from typing import Optional
+
 from presets import managePresets
+
+# Pfade könnten auch als Path objekte implementiert werden
 terrariaConfigDir = 'C:\\Users\\Joris\\Documents\\My games\\Terraria\\'
 steamWorkshopDir = 'H:\\SteamLibrary\\steamapps\\workshop\\content\\105600'
 confPath = os.path.join(terrariaConfigDir, 'config.json')
 
 
-def getSafeFile(path: str) -> str:
-    with io.open(path, 'r', encoding='utf-8-sig') as unsafeFile:
+def get_safe_file(path: Path) -> str:
+    with path.open('r', encoding='utf-8-sig') as unsafeFile:
         fileStub = []
         for line in unsafeFile:
             fileStub.append(line)
             if 'Name' in line:
                 break
-        return ''.join(fileStub)+'"Dummy": ""}'
-    
+        return ''.join(fileStub) + '"Dummy": ""}'
 
-def listPacks(active: bool):
+
+def load_conf_as_json() -> dict:
+    if not os.path.exists(confPath):
+        raise FileNotFoundError(f'ERROR: Config file not found at {confPath}')
+
+    with open(confPath, 'r') as configFp:
+        return json.load(configFp)
+
+
+def list_packs(active: bool):
+    jsonData = load_conf_as_json()
+
     print('Printing Active Packs')
-    if os.path.exists(confPath):
-        print(f'Order\t- ID      \t- Name')
-        config_fp = open(confPath, 'r')
-        jsonData = json.load(config_fp)
-        config_fp.close()
-        pathType = ""
-        #print(jsonData['ResourcePacks'])
-        for pack in jsonData['ResourcePacks']:
-            if pack['Enabled'] is active:
-                name = pack['FileName']
-                packName = name
-                jsonName = 'pack.json' if os.path.exists(os.path.join(steamWorkshopDir,name,'pack.json')) else 'Pack.json'
-                steamDir = os.path.join(steamWorkshopDir,name,jsonName)
-                localDir = os.path.join(terrariaConfigDir, 'ResourcePacks', name)
-                if os.path.exists(steamDir):
-                    #print(steamDir)
-                    #packName = json.load(open(steamDir, 'r'))['Name']
-                    fixedFile = getSafeFile(steamDir)
-                    pathType = packName
-                    try:
-                        packName = json.loads(fixedFile)['Name']
-                    except:
-                        print(f'ERROR: Name of pack {packName} could not be read.')
-                    #packName = json.load(io.open(steamDir, 'r', encoding='utf-8-sig'),strict=False)['Name']
-                elif os.path.exists(localDir):
-                    jsonName = 'pack.json' if os.path.exists(os.path.join(localDir,'pack.json')) else 'Pack.json'
-                    if os.path.exists(os.path.join(localDir,jsonName)):
-                        fixedFile = getSafeFile(os.path.join(localDir,jsonName))
-                        pathType = "LOCAL00000"
-                        try:
-                            packName = json.loads(fixedFile)['Name']
-                        except:
-                            print(f'ERROR: Name of pack {packName} could not be read.')
-                        #packName = json.load(io.open(os.path.join(terrariaConfigDir,'ResourcePacks',name,jsonName), 'r', encoding='utf-8-sig'),strict=False)['Name']                
-                else:
-                    print(f'(ERROR): {name} not found on disk')
-                    continue
-                print(f'#{pack['SortingOrder']}\t- {pathType}\t- {packName}')
+    print(f'Order\t- ID      \t- Name')
 
-def packReorder():
-    listPacks(True)
+    for pack in sorted(jsonData['ResourcePacks'], key=lambda x: x['SortingOrder']):
+        error = None
+        if pack['Enabled'] is not active:
+            continue
+        directoryName = pack['FileName']
+        packName = pack['FileName']
+        steamDir = Path(steamWorkshopDir, directoryName)
+        localDir = Path(terrariaConfigDir, 'ResourcePacks', directoryName)
+        packDir = None
+        pathType = packName
+        if steamDir.exists():
+            packDir = steamDir
+        elif localDir.exists():
+            packDir = localDir
+            pathType = "LOCAL00000"
+
+        if packDir is not None:
+            pack_file_path = get_pack_json_path(packDir)
+            if pack_file_path is None:
+                error = f'ERROR: pack.json for {packName} not found.'
+            else:
+                try:
+                    fixedFile = get_safe_file(pack_file_path)
+                    packName = json.loads(fixedFile)['Name']
+                except:
+                    error = f'ERROR: Name of pack {packName} could not be read.'
+        else:
+            error = f'ERROR: {directoryName} not found on disk'
+
+        print(f'#{pack['SortingOrder']}\t- {pathType}\t- {packName}')
+        if error is not None:
+            print(error)
+
+
+def get_pack_json_path(pack_path: Path) -> Optional[Path]:
+    pack_file_path = pack_path / 'pack.json'
+    if not pack_file_path.exists():
+        pack_file_path = pack_path / 'Pack.json'
+    if not pack_file_path.exists():
+        return None
+    return pack_file_path
+
+
+def pack_reorder():
+    list_packs(True)
     print('')
     print('Current position: ')
     old = int(input())
     print('New position: ')
     new = int(input())
-    #if old > new
+    # if old > new
     #    if x < old and x >= new -> x=x+1
-    #if old < new
+    # if old < new
     #    if x > old and x <= new -> x=x-1
     if old == new:
         return
@@ -82,50 +103,55 @@ def packReorder():
                     if x == old:
                         val = new
                     elif old > x >= new:
-                        val = x+1
-                    else: continue
-                else: #if old < new
+                        val = x + 1
+                    else:
+                        continue
+                else:  # if old < new
                     if x == old:
                         val = new
                     elif old < x <= new:
                         val = x - 1
-                    else: continue
+                    else:
+                        continue
                 pack.update({'SortingOrder': val})
                 print(f'Was at {x}\tmoved to {val}\t- {pack['FileName']}')
-        config_fp = open(confPath, 'w')
-        json.dump(jsonData,config_fp,indent=4)
-        config_fp.close()
+        with open(confPath, 'w') as config_fp:
+            json.dump(jsonData, config_fp, indent=4)
     return
 
-def packDeactivate():
+
+def pack_deactivate():
     return
 
-def packActivate():
+
+def pack_activate():
     return
+
 
 def backup_config():
-    os.popen(f'copy \"{confPath}\" \"{os.path.join(terrariaConfigDir,'config.json.bckp')}\"')
+    os.popen(f'copy \"{confPath}\" \"{os.path.join(terrariaConfigDir, 'config.json.bckp')}\"')
     if not os.path.exists('conf.json'):
         open('conf.json', 'w').write('{}')
 
 def start():
     backup_config()
-    listPacks(True)
+    list_packs(True)
     while True:
         print('Welcome to TRPH!')
-        #print('[1] => List Active Packs\n[2] => Reorder Pack\n[3] => Deactivate Pack\n[4] => List Inactive Packs\n[5] => Activate Pack\n[6] => Exit')
-        print('[1] => List Active Packs\n[2] => Reorder Pack\n[4] => List Inactive Packs\n[6] => EXIT\n[7] => Manage Presets')
+        # print('[1] => List Active Packs\n[2] => Reorder Pack\n[3] => Deactivate Pack\n[4] => List Inactive Packs\n[5] => Activate Pack\n[6] => Exit')
+        print(
+            '[1] => List Active Packs\n[2] => Reorder Pack\n[4] => List Inactive Packs\n[6] => EXIT\n[7] => Manage Presets')
         prompt = input('> ')
         if prompt == '1':
-            listPacks(True)
+            list_packs(True)
         elif prompt == '2':
-            packReorder()
+            pack_reorder()
         elif prompt == '3':
-            packDeactivate()
+            pack_deactivate()
         elif prompt == '4':
             listPacks(False)
         elif prompt == '5':
-            packActivate()
+            pack_activate()
         elif prompt == '6':
             exit()
         elif prompt == '7':
@@ -133,4 +159,6 @@ def start():
         else:
             print('(ERROR) Invalid option passed, exiting.')
             exit()
+
+
 start()
